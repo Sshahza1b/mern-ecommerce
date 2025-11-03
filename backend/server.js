2d343c55ecdeeb1da4c5cc5c80c1a33679766d44
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-03 11:48:15 +0500

    Fix PathError for wildcard route in Express

--- backend/server.js
+++ backend/server.js
@@ -53,9 +53,11 @@ app.listen(
 );
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
+  app.get('/*path', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
 }
+
+
